d63498c05a1b2629a60ba9f774bea45489042722
nMaax <carli.massimiliano01@gmail.com>
2024-12-21 14:22:49 +0100

    fixed hess powell def

--- num/test_problems_for_unconstrained_optimization/extended_powell_hess.m
+++ num/test_problems_for_unconstrained_optimization/extended_powell_hess.m
@@ -18,14 +18,15 @@ function HessF = extended_powell_hess(x, alpha)
     HessF = zeros(n, n);
 
     % Compute the gradient
-    for i = 1:n
-    
+    for i = 1:2:n-1
+                
         % Diagonal elements
-        HessF(i, i) = 0.5 * ext(-x(i));
-    
-        % Off-diagonal elements (symm. tri-diagonal)
-        temp = 0.5 * alpha;
-        HessF(i, i+1) = temp;
+        HessF(i, i)     = (alpha*x(i+1))^2 + 2*exp(-2*x(i)) + exp(-x(i))*(exp(-x(i+1))-gamma); % Odd (i)
+        HessF(i+1, i+1) = (alpha*x(i))^2 + 2*exp(-2*x(i+1)) + exp(-x(i+1))*(exp(-x(i))-gamma); % Even (i+1)
+        
+        % Off-diagonal elements (symm. tri-diagonal) Note: temp is off-diag of i, off-diag of i+1 is 0
+        temp = 2*alpha^2*x(i)*x(i+1) - alpha*beta + exp(-x(i))*exp(-x(i+1));
         HessF(i+1, i) = temp;
+        HessF(i, i+1) = temp;
     end
 end
