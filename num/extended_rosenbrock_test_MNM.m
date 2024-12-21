0e75bea64a17f77e62b9855015f46f7d19c1423a
nMaax <carli.massimiliano01@gmail.com>
2024-12-21 18:09:10 +0100

    renamed chain rosen test, defined extend rosen

---
+++ num/extended_rosenbrock_test_MNM.m
@@ -0,0 +1,47 @@
+%% Initialization
+% Clear the workspace and command window
+clc; clear; close all;
+
+% Add path to functions
+addpath('test_problems_for_unconstrained_optimization\'); 
+
+% Parameters
+rho = 0.7;        % Backtracking reduction factor
+c1 = 1e-2;        % Armijo condition parameter
+tolgrad = 1e-6;   % Gradient tolerance for stopping
+toleig = 1e-6;    % Tolerance for eigenvalue check
+btmax = 50;       % Maximum backtracking steps
+kmax = 5000;      % Maximum iterations
+
+% Problem size
+n = 1e3; % 4, 5
+
+% Starting points
+x_bar = [-1.2; 1];
+x0 = repmat(x_bar, n / size(x_bar, 1), 1);
+
+%% Define Rosenbrock Function, Gradient, and Hessian
+% Function
+f = @(x) extended_rosenbrock(x);
+
+% Gradient
+gradf = @(x) extended_rosenbrock_grad(x);
+
+% Hessian
+Hessf = @(x) extended_rosenbrock_hess(x);
+
+%% Test the Modified Newton's Method - Starting Point x_bar
+fprintf('Test Modified Newton Method on Chained Rosenbrock, n = %d\n', n);
+
+[xk1, fk1, gradfk_norm1, k1, xseq1, btseq1] = ...
+    modifiedNM(x0, f, gradf, Hessf, ...
+    toleig, kmax, tolgrad, c1, rho, btmax);
+
+% Display results
+fprintf('Function Value: %e\n', fk1);
+fprintf('Gradient Norm: %e\n', gradfk_norm1);
+fprintf('Iterations: %d\n', k1);
+fprintf('\n');
+
+%% End
+fprintf('Test Completed!\n');
