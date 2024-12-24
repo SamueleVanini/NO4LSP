8eba048e3007cce1686ac69eddbcc11eb17d2995
nMaax <carli.massimiliano01@gmail.com>
2024-12-24 17:17:40 +0100

    Enhance bunch_parlett_criteria to return pivot indices and update pivot selection logic

--- num/matrix_corrections/bunch_parlett_criteria.m
+++ num/matrix_corrections/bunch_parlett_criteria.m
@@ -1,4 +1,4 @@
-function [E] = bunch_parlett_criteria(A, growth_threshold)
+function [E, i, j] = bunch_parlett_criteria(A, growth_threshold)
 
     if nargin < 2
         growth_threshold = .5;
@@ -12,16 +12,20 @@ function [E] = bunch_parlett_criteria(A, growth_threshold)
      [dia, dia_index] = max(abs(diag(A)));
     
      % Find the largest offd-diagonal element magnitude (offd)
-     [offd, linear_index] = max(abs(triu(A, 1)), [], 'all');
-     [i, j] = ind2sub(size(A), linear_index); % Convert linear index to subscripts
+     [offd, offd_linear_index] = max(abs(triu(A, 1)), [], 'all');
+     [offd_index_i, offd_index_j] = ind2sub(size(A), offd_linear_index); % Convert linear index to subscripts
  
      %? TODO: what does it mean to the ratio to be acceptabe, < or > ? 
      % Decide on the pivot block
      if dia / offd >= growth_threshold
          % Select the diagonal element as a 1x1 pivot block
-         E = A(dia_index, dia_index);
+         i = dia_index;
+         j = dia_index;
+         E = A(i, j);
      else
          % Select the 2x2 submatrix as the pivot block
+         i = offd_index_i;
+         j = offd_index_j;
          E = A([i, j], [i, j]);
      end
 end
