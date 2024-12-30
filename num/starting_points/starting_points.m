b6f54631a4e2e3f4ce93ee1537912efb91c87f64
Ciovino <tarantino.giovanbattista01@gmail.com>
2024-12-30 17:12:04 +0100

    removed chained rosenbrock starting points

--- num/starting_points/starting_points.m
+++ num/starting_points/starting_points.m
@@ -4,42 +4,32 @@ clc
 seed = min([318684, 337728, 338137]);
 problem_dim = [1e3 1e4 1e5];
 
-%% Chained Rosenbrock
-for n = problem_dim
-    x_0 = repmat([-1.2; 1], floor(n/2), 1);
-
-    all_x = create_points(seed, x_0);
-
-    file_name = sprintf("%d-dim_ChainRos.mat", n);
-    save(file_name, "x_0", "all_x");
-end
-
 %% Extended Rosenbrock
 for n = problem_dim
     x_0 = repmat([-1.2; 1], floor(n/2), 1);
 
-    all_x = create_points(seed, x_0);
+    all_x = [x_0, create_points(seed, x_0)];
 
     file_name = sprintf("%d-dim_ExtRos.mat", n);
-    save(file_name, "x_0", "all_x");
+    save(file_name, "all_x");
 end
 
 %% Extended Powell
 for n = problem_dim
     x_0 = repmat([0; 1], floor(n/2), 1);
 
-    all_x = create_points(seed, x_0);
+    all_x = [x_0, create_points(seed, x_0)];
 
     file_name = sprintf("%d-dim_ExtPow.mat", n);
-    save(file_name, "x_0", "all_x");
+    save(file_name, "all_x");
 end
 
 %% Problem 82
 for n = problem_dim
     x_0 = 0.5*ones(n, 1);
 
-    all_x = create_points(seed, x_0);
+    all_x = [x_0, create_points(seed, x_0)];
 
     file_name = sprintf("%d-dim_Prob82.mat", n);
-    save(file_name, "x_0", "all_x");
+    save(file_name, "all_x");
 end
