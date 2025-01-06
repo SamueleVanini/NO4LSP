6e3c9607fb55e07df2d5461b45756a565fd84acd
Ciovino <tarantino.giovanbattista01@gmail.com>
2025-01-06 19:03:50 +0100

    added global minimum

--- num/starting_points/compact_starting_points.m
+++ num/starting_points/compact_starting_points.m
@@ -7,12 +7,13 @@ problem_dim = [1e3 1e4 1e5];
 %% Rosenbrock
 % Problem dimension -> n = 2
 x_2 = [1.2, 1.2; -1.2, 1]';
+minimum = [1; 1];
 
 f = @rosenbrock;
 grad_f = @rosenbrock_grad;
 hess_f = @rosenbrock_hess;
 
-save('Rosenbrock.mat', "x_2", "f", "grad_f", "hess_f");
+save('Rosenbrock.mat', "x_2", "f", "grad_f", "hess_f", "minimum");
 
 %% Extended Rosenbrock
 file_name = 'Ext_Rosenbrock.mat';
@@ -27,16 +28,20 @@ x_0 = [-1.2; 1];
 % Problem dimension -> 1000
 x_0 = repmat(x_0, 500, 1);
 x_1000 = [x_0, create_points(seed, x_0)];
+min_1000 = ones(1000, 1);
 
 % Problem dimension -> 10000
 x_0 = repmat(x_0, 10, 1);
 x_10000 = [x_0, create_points(seed, x_0)];
+min_10000 = ones(10000, 1);
 
 % Problem dimension -> 100000
 x_0 = repmat(x_0, 10, 1);
 x_100000 = [x_0, create_points(seed, x_0)];
+min_100000 = ones(100000, 1);
 
-save(file_name, "x_1000", "x_10000", "x_100000", "f", "grad_f", "hess_f", "hess_approx");
+save(file_name, "x_1000", "x_10000", "x_100000", "f", "grad_f", "hess_f",...
+    "hess_approx", "min_1000", "min_10000", "min_100000");
 
 %% Extended Powell
 file_name = 'Ext_Powell.mat';
@@ -47,22 +52,27 @@ hess_f = @extended_powell_hess;
 hess_approx = @extended_powell_hess_approx;
 
 x_0 = [0; 1];
+minimum = [1.09815933e-5; 9.106146738]; % f(min) = 2.3e-21; norm of grad = 6.2e-6
 
 % Problem dimension -> 1000
 x_0 = repmat(x_0, 500, 1);
+min_1000 = repmat(minimum, 500, 1);
 x_1000 = [x_0, create_points(seed, x_0)];
 
 % Problem dimension -> 10000
 x_0 = repmat(x_0, 10, 1);
+min_10000 = repmat(minimum, 5000, 1);
 x_10000 = [x_0, create_points(seed, x_0)];
 
 % Problem dimension -> 100000
 x_0 = repmat(x_0, 10, 1);
+min_100000 = repmat(minimum, 50000, 1);
 x_100000 = [x_0, create_points(seed, x_0)];
 
-save(file_name, "x_1000", "x_10000", "x_100000", "f", "grad_f", "hess_f", "hess_approx");
+save(file_name, "x_1000", "x_10000", "x_100000", "f", "grad_f", "hess_f",...
+    "hess_approx", "min_1000", "min_10000", "min_100000");
 
-%% Extended Powell
+%% Problem 82
 file_name = 'Problem_82.mat';
 
 f = @problem_82;
@@ -72,14 +82,18 @@ hess_approx = @problem_82_hess_approx;
 
 % Problem dimension -> 1000
 x_0 = .5*ones(1000, 1);
+min_1000 = zeros(1000, 1);
 x_1000 = [x_0, create_points(seed, x_0)];
 
 % Problem dimension -> 10000
 x_0 = .5*ones(10000, 1);
+min_10000 = zeros(10000, 1);
 x_10000 = [x_0, create_points(seed, x_0)];
 
 % Problem dimension -> 100000
 x_0 = .5*ones(100000, 1);
+min_100000 = zeros(100000, 1);
 x_100000 = [x_0, create_points(seed, x_0)];
 
-save(file_name, "x_1000", "x_10000", "x_100000", "f", "grad_f", "hess_f", "hess_approx");
+save(file_name, "x_1000", "x_10000", "x_100000", "f", "grad_f", "hess_f",...
+    "hess_approx", "min_1000", "min_10000", "min_100000");
