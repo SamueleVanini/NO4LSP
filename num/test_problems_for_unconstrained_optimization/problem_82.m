a8b703a363a08c14580af73cf2360cdc0a01ab52
nMaax <carli.massimiliano01@gmail.com>
2025-01-13 19:06:38 +0100

    cleaned problem 82 functions definition improving comments, indent

--- num/test_problems_for_unconstrained_optimization/problem_82.m
+++ num/test_problems_for_unconstrained_optimization/problem_82.m
@@ -1,18 +1,19 @@
 function F = problem_82(x)
-%PROBLEM_82 Problem 82 function evaluation
-% Input:
-%   x : n-dimensional vector
-% Output:
-%   F : scalar function value
+    %PROBLEM_82 Problem 82 function evaluation
+    % Input:
+    %   x : n-dimensional vector
+    % Output:
+    %   F : scalar function value
 
-n = length(x);
-cos_x = cos(x);
+    n = length(x);
+    cos_x = cos(x);
 
-% first term
-F = x(1)^2; 
-for i = 2:n
-    F = F + (cos_x(i-1) + x(i) - 1)^2;
-end
+    % First term
+    F = x(1)^2;
+    for i = 2:n
+        F = F + (cos_x(i-1) + x(i) - 1)^2;
+    end
 
-F = .5*F;
+    % Divide by two
+    F = 0.5*F;
 end
--- num/test_problems_for_unconstrained_optimization/problem_82_hess_approx.m
+++ num/test_problems_for_unconstrained_optimization/problem_82_hess_approx.m
@@ -1,60 +1,61 @@
 function Hess = problem_82_hess_approx(x_bar, h, specific, gradF, gradF_xbar)
-%PROBLEM_82_HESS_APPROX Approximation of the Jacobian of the Problem 82
-%function
-%   For specific tri-diagonal jacobian
-%
-%   Input:
-%       x_bar - point in where to compute the jacobian
-%           row vector
-%       h - level of approximation
-%           scalar
-%       specific - implement a specific approximation based on x_bar
-%           logical value
-%       gradF - gradient of the Problem 82 function
-%           function handle
-%       gradF_xbar - gradient at x_bar
-%           row vector
-%
-%   Output
-%       Hess - approximation of the hessian
-%           sparse matrix (tri-diagonal)
+    %PROBLEM_82_HESS_APPROX Approximation of the Jacobian of the Problem 82
+    %function
+    %   For specific tri-diagonal jacobian
+    %
+    %   Input:
+    %       x_bar - point in where to compute the jacobian
+    %           row vector
+    %       h - level of approximation
+    %           scalar
+    %       specific - implement a specific approximation based on x_bar
+    %           logical value
+    %       gradF - gradient of the Problem 82 function
+    %           function handle
+    %       gradF_xbar - gradient at x_bar
+    %           row vector
+    %
+    %   Output
+    %       Hess - approximation of the hessian
+    %           sparse matrix (tri-diagonal)
 
-% problem dimension
-n = length(x_bar);
+    % Problem dimension
+    n = length(x_bar);
 
-% gradient at x_bar
-if isempty(gradF_xbar)
-    gradF_xbar = gradF(x_bar);
-end
+    % Gradient at x_bar
+    if isempty(gradF_xbar)
+        gradF_xbar = gradF(x_bar);
+    end
 
-% approximation step for each component of x
-if specific
-    h_vec = h*abs(x_bar);
-else
-    h_vec = h*ones(n, 1);
-end
+    % Approximation step for each component of x
+    if specific
+        h_vec = h*abs(x_bar);
+    else
+        h_vec = h*ones(n, 1);
+    end
 
-% perturbations vectors
-e1 = zeros(n, 1);
-e2 = zeros(n, 1);
-e3 = zeros(n, 1);
+    % Perturbations vectors
+    e1 = zeros(n, 1);
+    e2 = zeros(n, 1);
+    e3 = zeros(n, 1);
 
-e1(1:3:n) = h_vec(1:3:n);
-e2(2:3:n) = h_vec(2:3:n);
-e3(3:3:n) = h_vec(3:3:n);
+    e1(1:3:n) = h_vec(1:3:n);
+    e2(2:3:n) = h_vec(2:3:n);
+    e3(3:3:n) = h_vec(3:3:n);
 
-% compute hessian component
-approx = @(e) (gradF(x_bar + e) - gradF_xbar)./h_vec;
-eval = [approx(e1), approx(e2), approx(e3)];
+    % Compute hessian component
+    approx = @(e) (gradF(x_bar + e) - gradF_xbar)./h_vec;
+    eval = [approx(e1), approx(e2), approx(e3)];
 
-% build hessian
-row_idx = (2:n)';
-col_idx = mod(row_idx - 1, 3) + 1;
+    % Hessian indices for sparsity
+    row_idx = (2:n)';
+    col_idx = mod(row_idx - 1, 3) + 1;
+    
+    % Build hessian
+    main_diag = [eval(1, 1); eval(sub2ind(size(eval), row_idx, col_idx))];
+    off_diag = eval(sub2ind(size(eval), row_idx - 1, col_idx));
 
-main_diag = [eval(1, 1); eval(sub2ind(size(eval), row_idx, col_idx))];
-off_diag = eval(sub2ind(size(eval), row_idx - 1, col_idx));
-
-% build a sparse matrix
-Bin = [[off_diag; 0], main_diag, [0; off_diag]];
-Hess = spdiags(Bin, [-1 0 1], n, n);
+    % Build a sparse matrix
+    Bin = [[off_diag; 0], main_diag, [0; off_diag]];
+    Hess = spdiags(Bin, [-1 0 1], n, n);
 end
