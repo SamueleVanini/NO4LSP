- fixed value iteration with finite time horizon - optimized code for iteration computation (action probabilities also in a matrix) - added hyperparameters in customers simulator trace log - rewritten model - added policy extraction

stoc/solvers/agent.py
@@ -2,89 +2,81 @@
 Module to provide classes and functions to create an agent for a Markov decision process
 """
 
-from typing import Optional
+import math
+from typing import Any
+import scipy
 import numpy as np
-import numpy.typing as npt
+from tqdm import tqdm
 from .environment import AirlineModel
-from simulator.clients_traces import Customer
 
 
-class FiniteHorizonValueIteration:
+class Agent:
 
-    def __init__(
-        self,
-        model: AirlineModel,
-        vTable: Optional[npt.NDArray[np.float64]] = None,
-        qTable: Optional[npt.NDArray[np.float64]] = None,
-    ) -> None:
-        assert (vTable is None and qTable is None) or (
-            vTable is not None and qTable is not None
-        ), "Error: you must provide both vTable and qTable or neither"
+    def __init__(self, discount: float, theta: float, model: AirlineModel, customersParams: dict[str, Any]):
+        self.discount = discount
+        self.theta = theta
         self.model = model
-        if vTable and qTable:
-            self.vTable = vTable.copy()
-            self.qTable = qTable.copy()
-        else:
-            self.vTable, self.qTable = self._init_tables()
+        self.lambd = float(customersParams["lambd"])
+        self.alpha = int(customersParams["alpha"])
+        self.beta = int(customersParams["beta"])
+        self.V = np.zeros((model.tot_tickets + 1, model.nbins_price, model.horizon + 1))
+        self.R = np.zeros((model.tot_tickets + 1, model.nbins_price))
+        self.T = np.zeros((model.nbins_price, model.tot_tickets + 1, model.tot_tickets + 1))
+        self._init_tables()
 
-    def _init_tables(self) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
-        V = np.zeros((self.model.nmax_tickets, self.model.nmax_days), dtype=np.float64)
-        Q = np.zeros(
-            (self.model.nmax_tickets, self.model.nbins_price, self.model.nmax_days),
-            dtype=np.float64,
-        )
-        for tickets_left in range(self.model.nmax_tickets):
-            for demand_index, price_level in enumerate(self.model.price_levels):
-                # TODO: done is not used check if we can reduce the number of computation using it
-                reward, ticket_sold, _ = self.model.step((tickets_left, price_level, 0))
-                Q[tickets_left, demand_index, 0] = reward
-            best_revenue_per_level = Q[tickets_left, :, 0].max(axis=0)
-            # TODO: check if the mean if correct for the expected reward since the probability in the price levels is Beta distributed
-            V[tickets_left, 0] = best_revenue_per_level.mean()
+    def _init_tables(self):
 
-        return V.copy(), Q.copy()
+        ncustomers = math.ceil(1 / self.lambd)
 
-    def solve(self):
-        for days_left in range(1, self.model.nmax_days):
-            for tickets_left in range(self.model.nmax_tickets):
-                for demand_index, price_level in enumerate(self.model.price_levels):
-                    reward, tickets_sold, _ = self.model.step((tickets_left, price_level, days_left))
-                    self.qTable[tickets_left, demand_index, days_left] = (
-                        reward + self.vTable[tickets_left - tickets_sold, days_left - 1]
-                    )
-                expected_reward_at_levels = self.qTable[tickets_left, :, days_left].max(axis=0)
-                # TODO: check if the mean if correct for the expected reward since the probability in the price levels is Beta distributed
-                self.vTable[tickets_left, days_left] = expected_reward_at_levels.mean()
+        for price_bin in range(self.model.nbins_price):
+            # remove the 0.0001 and properly fix it
+            p = scipy.stats.beta.sf(self.model.price_levels_scaled[price_bin], self.alpha, self.beta)
+            for s in range(self.model.tot_tickets + 1):
+                # prob_to_sell_k_tickets = [scipy.special.binom(ncustomers, k) * (p ^ k) * ((1 - p) ^ k) for k in range(0, s)]
+                prob_to_sell_k_tickets = np.zeros((self.model.tot_tickets + 1))
+                for k in range(0, s + 1):
+                    prob = scipy.special.binom(ncustomers, k) * (p**k) * ((1 - p) ** (ncustomers - k))
+                    prob_to_sell_k_tickets[k] = prob
+                exp = np.exp(prob_to_sell_k_tickets[: s + 1])
+                self.T[price_bin, : s + 1, s] = exp / sum(exp)
+                # check if ncustomers can be greater than s
+                self.R[s, price_bin] = sum(
+                    [self.model.price_levels[price_bin] * k * prob_to_sell_k_tickets[k] for k in range(s + 1)]
+                )
 
-    def score_greedy_policy(self, trace: list[Customer]):  # type: ignore
-        ticket_left = self.model.nmax_tickets - 1
+        for s in range(self.model.tot_tickets + 1):
+            self.V[s, :, 0] = self.model.seat_fixed_cost * s
 
-        revenue, ticket_sold = self._step_greedy(ticket_left, self.model.nmax_days - 1, trace)
-        return revenue, ticket_sold
+    def solve(self, max_iter: int = 1000, theta: float = 0.01):
+        # delta = 0
 
-    def _step_greedy(self, ticket_left, days_left, trace, ticket_sold=0):
-        if days_left == -1 or ticket_left == -1:
-            return 0, 0
-        relevant_Q_vals = self.qTable[ticket_left, :, days_left]
-        price_bin_idx = relevant_Q_vals.argmax()
-        price = self.model.price_levels[price_bin_idx]
-        ticket_sold = self._get_ticket_sold(
-            ticket_left, price, [customer for customer in trace if customer.arrival_day == days_left]
-        )
-        reward = price * ticket_sold
-        ticket_left -= ticket_sold
-        new_step_reward, new_ticket_sold = self._step_greedy(ticket_left, days_left - 1, trace, ticket_sold)
-        return (reward + new_step_reward), (ticket_sold + new_ticket_sold)
+        # v = self.V.copy()
+        for days_left in tqdm(range(1, self.model.horizon + 1)):
+            for t in range(self.model.tot_tickets + 1):
+                for b in range(self.model.nbins_price):
+                    current_reward = self.R[t, :]
+                    potential_reward = self.discount * (self.V[:, b, days_left - 1] @ self.T[:, :, t].T)
+                    tot_rewards = current_reward + potential_reward
+                    self.V[t, b, days_left] = tot_rewards.max()
+        # a = self.V - v
+        # intermediate_norm = np.linalg.norm(a, ord=np.inf, axis=(0, 1))
+        # delta = np.linalg.norm(intermediate_norm[1:], ord=np.inf)
 
-    def _get_ticket_sold(self, max_ticket, price, trace):
-        ticket_sold = 0
-        value_scaled = (price - self.model.min_price) / (self.model.max_price - self.model.min_price)
-        if max_ticket == 0:
-            return 0
-        for customer in trace:
-            if max_ticket == 0:
-                break
-            if value_scaled <= customer.percived_value:
-                ticket_sold += 1
-                max_ticket -= 1
-        return ticket_sold
+        # if delta < theta:
+        # break
+
+    def get_policy(self) -> dict[tuple[int, int, int], int]:
+        # ticket left, price bin, day left => next price bin
+        policy: dict[tuple[int, int, int], int] = {}
+        # 3 for loops to cicly on all the states
+        for day in range(self.model.horizon, 0, -1):
+            for price_bin in range(self.model.nbins_price):
+                for ticket_left in range(self.model.tot_tickets + 1):
+                    current_reward = self.R[ticket_left, :]
+                    potential_reward = self.discount * (self.V[:, price_bin, day - 1] @ self.T[:, :, ticket_left].T)
+                    tot_rewards = current_reward + potential_reward
+                    best_bin = tot_rewards.argmax()
+                    # policy[(ticket_left, price_bin, day)] = self.model.price_levels[best_bin]
+                    policy[(ticket_left, price_bin, day)] = best_bin  # type: ignore
+
+        return policy